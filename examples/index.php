b8134d2908e4056b52a3ec35b93a10967d24bb53
jhdxr <me@jhdxr.com>
2016-08-14 01:27:41 +0800

    add composer support

--- examples/index.php
+++ examples/index.php
@@ -1,5 +1,6 @@
 <?php
-	require_once 'Client.php';
+	
+require_once(dirname(__DIR__).'/vendor/autoload.php');
 	
 //KF云客服事务工单系统APIV2调用示例：
 	
@@ -11,7 +12,7 @@
 	
 	
 //二、实例化调用类
-	//$test = new Client($yourDomain, $email);
+	$test = new Kf5Client($yourDomain, $email);
 	
 //三、根据你调用不同的API接口，设置不同的访问权限方式
 	/* APIV2调用权限分为三种：admin 表示管理员，agent 表示客服，end user 表示普通用户
--- src/Client.php
+++ src/Client.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'core/apiRequire.php';
-class Client{
+class Kf5Client{
 	
 	protected  $subdomain;
 	protected  $email;
